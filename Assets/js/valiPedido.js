c855559e61161b30f3f7b5b8ddb63923d6a048df
BaGutti <brianalveirog@gmail.com>
2020-09-10 15:21:29 -0500

    Confirmación pedido completa y arreglos

--- Assets/js/valiPedido.js
+++ Assets/js/valiPedido.js
@@ -1,12 +1,12 @@
 function validacion() {
   if (localStorage.getItem("carrito") == "[]") {
 
-    Push.create("Carrito", {
+    Push.create("RPPSQUIMICOS", {
     body: "Seleccione un producto antes de continuar",
     icon: 'Assets/img/logo2.png',
     timeout: 4000,
     onClick: function () {
-        window.focus();
+        window.location="index.php?paginasProduc=Catalog";
         this.close();
     }
     });
--- Views/paginasPedidos/confirmacion.php
+++ Views/paginasPedidos/confirmacion.php
@@ -16,12 +16,39 @@
         </div>
     </div>
 </section>
-<div style="padding-left: 15em; padding-right: 13em; padding-top: 5em; padding-bottom: 15em">
-      <h1 class="text-center">¿Todo está listo?</h1>
+
+<div style="padding-left: 20em; padding-right: 20em; padding-top: 5em; padding-bottom: 5em">
+      <div class="border border-dark rounded-sm" style="padding: 3em">
+      <h1 class="text-center">¿Está seguro de Completar el Pedido?</h1>
       <input type="hidden" name="fechaEntre" id="FechaEntrega" value="<?php echo date("Y-m-d",$mod_date) . "\n";?>">
       <input type="hidden" name="idClient" id="IdCliente" value="<?php echo $user["idEC_FK"]; ?>">
       <input type="hidden" id="totalCart" value="<?php echo $totalcarr;?>">
-      <button type="button" class="btn btn-primary" id="alerta" name="button"> Si </button>
-      <button type="button" class="btn btn-danger" id="alerta" name="button"> No </button>      
+      <div class="d-flex justify-content-center" style="padding-top: 4em">
+        <button type="button" style="margin: 1em" class="btn btn-info btn-lg btn-block" id="alerta" name="button" data-toggle="modal" data-target="#Confirma"> ¡Claro! <i class="far fa-check-circle"></i> </button>
+
+        <a href="index.php?paginasProduc=Catalog" style="margin: 1em" class="btn btn-secondary btn-lg btn-block" id="alerta" name="button"> Aún faltan cosas <i class="far fa-clock"></i></a>
+      </div>
+    </div>
 </div>
-<div id="productoDetallado"></div>
+
+<!-- Modal -->
+<div class="modal fade" id="Confirma" data-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">¡Pedido Completo!</h5>
+          <!--<span aria-hidden="true">&times;</span>-->
+        </button>
+      </div>
+      <div class="modal-body">
+          <p>Felicidades el pedido se registró correctamente.</p> <br>
+          <p>Se envió un email con la información correspondiente a <h4><strong><?php echo $user["nombreUSUARIO"]; ?> </strong><h4></p>
+
+      </div>
+      <div class="modal-footer">
+        <a href="index.php" class="btn btn-secondary">Volver al Inicio</a>
+        <a href="index.php?paginasPedidos=PedidosPersona" class="btn btn-info">Ver mis pedidos</a>
+      </div>
+    </div>
+  </div>
+</div>
